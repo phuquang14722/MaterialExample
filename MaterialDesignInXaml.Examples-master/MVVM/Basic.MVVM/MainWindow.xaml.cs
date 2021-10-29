be9001ffc2fe0fcf1460bd6954c9c2976adadbc1
Phu Quang <20521800@gm.uit.edu.vn>
2021-10-29 23:36:36 +0700

    Initial commit

---
+++ MaterialDesignInXaml.Examples-master/MVVM/Basic.MVVM/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Basic.MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            var viewModel = new ViewModel();
+            viewModel.FirstName = "Kevin";
+
+            DataContext = viewModel;
+            InitializeComponent();
+
+            viewModel.FirstName = "Mark";
+        }
+    }
+}
